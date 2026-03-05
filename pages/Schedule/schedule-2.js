ba6d105089a12019d8110cfcf42ac729abdc36fc
Tim Huynh <huynt553+round@gmail.com>
2026-03-04 19:09:41 -0800

    fix desktop schedule search by

--- pages/Schedule/schedule-2.js
+++ pages/Schedule/schedule-2.js
@@ -9,7 +9,7 @@ function myFunction() {
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
       var home = tr[i].getElementsByTagName("td")[0];
-      var away = tr[i].getElementsByTagName("td")[1];
+      var away = tr[i].getElementsByTagName("td")[2];
       if (home && away && !tr[i].classList.contains("weekRow")) {
         txtValue = home.textContent || home.innerText;
         txtValue2 = away.textContent || away.innerText;
